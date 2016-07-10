eb316708216fe49bd47229054a67e438bb43ab65
Viktor Mendes <vmj100@gmail.com>
2016-07-10 17:26:15 +0200

    added 1st file

---
+++ 1.strings.py
@@ -0,0 +1,16 @@
+
+print("House" + 'of cards')
+
+print("House", 'of cards')
+
+#print('I am number' + 5)
+
+print('I am number' , 5)
+
+print(5 , 'is my number')
+
+print("I'm 5")
+
+print("c:\\users\\settings\\text")
+
+print("c:/users/settings/text")
