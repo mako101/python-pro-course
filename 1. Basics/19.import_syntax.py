771d6c85e62381b49a6f7a19ceee7479094a317d
Viktor Mendes <vmendes@theworkshop.com>
2016-07-21 15:54:32 +0200

    import file

---
+++ 1. Basics/19.import_syntax.py
@@ -0,0 +1,40 @@
+#from statistics import *
+
+import statistics as stat
+
+from statistics import mean, stdev as s
+
+from statistics import variance as v
+
+myList = [5,5,12,74,85,1,7,4,58,587,8,8,8,5,9,7,744,7,85,12,5,]
+
+
+print(stat.median(myList))
+
+print('mean is', mean(myList))
+
+print('variance is', v(myList))
+
+print('standard deviation is', s(myList))
+
+#
+# for n in myList:
+#     print(n,end=' ')
+#
+
+#
+# print('median is', median(myList))
+#
+# print('mode is', mode(myList))
+#
+
+#
+
+#
+#
+#
+#
+#
+#
+
+
