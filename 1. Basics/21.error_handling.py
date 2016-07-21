86fb4eb4747b177879fcb504f767334f58a6b63c
Viktor Mendes <vmj100@gmail.com>
2016-07-21 22:21:00 +0200

    started error handling

---
+++ 1. Basics/21.error_handling.py
@@ -0,0 +1,30 @@
+type_error_list=[]
+try:
+    print("Doing some stuff ...")
+    # ooops!
+    print(1 + '1')
+    print(x)
+
+except TypeError as t:
+    type_error_list.append(t)
+
+if len(type_error_list) > 0:
+    print('The following Type Errors occured:')
+    for error in type_error_list:
+        print(error)
+
+
+
+try:
+    print("Doing some stuff ...")
+    # ooops!
+    print(x)
+
+except NameError as n:
+    print('Name Error found:', n)
+
+
+
+
+# except Exception as e:
+#     print(str(e))
