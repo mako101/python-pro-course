6476f0d344a572b38bc2760744bc497dabf58e16
Viktor Mendes <vmendes@theworkshop.com>
2016-08-05 16:37:57 +0200

    time stuff

--- complete-dev-course/76.time_datetime_named_tuples.py
+++ complete-dev-course/76.time_datetime_named_tuples.py
@@ -32,6 +32,29 @@ for i in (range(3)):
 from time import perf_counter as my_timer
 import random
 
-def game():
-    input('Press Enter to Start')
-    wait_time = random.rand
+# def game():
+#     input('Press Enter to Start')
+wait_time = random.randint(1,5)
+start_time = my_timer()
+#print(start_time)
+
+t.sleep(wait_time)
+end_time = my_timer()
+#print(end_time)
+
+print(t.localtime(start_time))
+print("Started at: " + t.strftime("%X", t.localtime(start_time)))
+print(t.localtime(end_time))
+print("Finished at: " + t.strftime("%X", t.localtime(end_time)))
+
+print("The interval was {:.4f} seconds".format(end_time - start_time))
+
+from time import perf_counter
+from time import monotonic
+from time import process_time
+
+print("\nVarious counters:")
+print("perf_counter:", perf_counter())
+print("monolithic:", monotonic())
+print("process_time: (time for CPU perform operations)", process_time())
+
