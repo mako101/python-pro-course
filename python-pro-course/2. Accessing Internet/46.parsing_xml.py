6ae45c27d77910c15b9109be7c222c51bd9db710
Viktor Mendes <vmj100@gmail.com>
2016-09-03 22:49:40 +0200

    html

--- python-pro-course/2. Accessing Internet/46.parsing_xml.py
+++ python-pro-course/2. Accessing Internet/46.parsing_xml.py
@@ -8,16 +8,14 @@ req = r.urlopen('http://www.linuxtoday.com/biglt.rss')
 xml = bs(req, 'xml')
 # print(xml)
 
-print(type(xml.findAll('item')))
 
-# for item in xml.findAll('link'):
-#     url = item.text  # extract just the content of tags
-#     print(20 * '#', '\n', url)
+# get just the links from the feed
+for item in xml.findAll('link'):
+    url = item.text  # extract just the content of tags
+    print(20 * '#', '\n', url)
 
 
 # processing each feed(item) by tags!
-
-
 items = xml.findAll('item')
 for item in items:
     # print(type(item))
@@ -27,48 +25,4 @@ for item in items:
     print('\tLink:', item.link.text)
     print('\tDate:', item.pubDate.text)
     print()
-
-
-channel = xml.findAll('channel')
-# print(channel_info) # ths returns the whole doc
-print(channel.index(1))
-
-# using lxml parser directly:
-#
-# all_feeds = xml.findAll('item')
-#
-# for item in all_feeds:
-#     # we need to convert the result set into string and then convert it into XML :-/
-#     item = str(item)
-#     print(item)
-#     feed = e.fromstring(item)
-#     print(feed)
-#     # Now we can pull individual elements out
-#     title = feed.find('title')
-#     link = feed.find('link')
-#     description = feed.find('description')
-#     date = feed.find('pubDate')
-#
-#     print(link.text)
-
-# print('\n', one_item, '\n')
-#
-# feed = e.fromstring(one_item)
-#
-#
-# print(type(link))
-#
-#
-# print(description.text)
-# print(link.text)
-
-
-
-# for item in list(xml.findAll('item')):
-#     #print(20 * '#', '\n', item)
-#     link = xml.findAll('link')
-#     #url = link.text
-#     description = xml.findAll('description')
-#     #desc = description.text
-#     print(20 * '#', '\n',description, '\n', link)
 
